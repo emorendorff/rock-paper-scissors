1f3d541e2bc5743d7e47a0efb1637abf77c54a51
Em Orendorff <em.orendorff@gmail.com>
2021-04-20 11:47:46 -0500

    Start Game class and add comment outlines to build js

---
+++ game.js
@@ -0,0 +1,10 @@
+class Game {
+
+}
+//two player instances
+//way to keep track of the data for the game board
+//way to keep track of the selected game type
+//way to keep tract of which players turn it is
+//a way to check the games board data for win conditions
+//way to detect when a gmae is a draw
+//way to reset the Game's board to begin a new game 
