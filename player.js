57f396baddd70734987f849959b311dd959d2549
Em Orendorff <em.orendorff@gmail.com>
2021-04-27 17:42:55 -0500

    Remove take turn function from player class

--- player.js
+++ player.js
@@ -3,17 +3,7 @@ class Player {
     this.name = name
     this.token = token
     this.wins = 0
-  }
-
-  takeTurn() {
-    if(!game.computerTurn) {
-      game.computerTurn = true;
-      game.humanTurn = false;
-    } else {
-      game.humanTurn
-      !game.computerTurn
-    }
-  }
+  };
 
   saveWinsToStorage() {
     localStorage.setItem(`${this.name}`, JSON.stringify(this.wins));
